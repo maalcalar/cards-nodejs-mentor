10a6b3fb5edd46e0fe0104f8b4fbc021d57fa37d
Miguel Angel Alcalá Rios <miguel.alcala.rios@gmail.com>
2024-02-14 15:12:19 -0500

    Added README

---
+++ README.m
@@ -0,0 +1,28 @@
+# Cards Filterer CLI
+
+A program to find brand from card number.
+
+## Requirements
+
+- NodeJS
+
+## Installation
+
+```sh
+npm install
+```
+
+## Commands
+
+```sh
+npm run start find <cardNumber>
+```
+
+### Example
+```sh
+npm run start find 6512345678901234
+> DISCOVER
+
+npm run start find 4512345678901234
+> VISA
+```
